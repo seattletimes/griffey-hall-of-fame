f6829356c16c58092a68cb832c1f906ba859584f
Elif Koc <elifkoc@nyu.edu>
2016-07-19 18:36:29 -0700

    Cards

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,11 @@ var baData = [0.264, 0.3, 0.327, 0.308, 0.309, 0.323, 0.258, 0.303, 0.304, 0.284
 
 var hrData = [16, 22, 22, 27, 45, 40, 17, 49, 56, 56, 48, 40, 22, 8, 13, 20, 35, 27, 30, 11, 19, 0];
 
+var slugData = [0.42, 0.481, 0.527, 0.535, 0.617, 0.674, 0.481, 0.628, 0.646, 0.611, 0.576, 0.556, 0.533, 0.426, 0.566, 0.513, 0.576, 0.486, 0.496, 0.424, 0.411, 0.204];
+
+var playData = [78, 96, 95, 88, 96, 99, 50, 86, 97, 99, 99, 90, 69, 43, 33, 51, 79, 67, 89, 88, 72, 20];
+
+var rbiData = [61, 80, 100, 103, 109, 90, 42, 140, 147, 146, 134, 118, 65, 23, 26, 60, 92, 72, 93, 71, 57, 7];
 
 //BATTING AVERAGE
 var battingAverage = {
@@ -36,17 +41,15 @@ var baChart =  Chartist.Line('.ct-averages', battingAverage, baOptions);
 
 //BA draw
 baChart.on('draw', function(data) {
-  var horizctr, vertctr, label, value;
-  if (data.type === "point") {
-
+ 
+  if (data.type === "point")
     //Change colors
     if (data.index < 11 || data.index > 18) {
       data.element.attr({
         style: 'stroke: blue;'
       });
     }
-  }
-});
+  });
 
 //BA interactives
 
@@ -72,6 +75,120 @@ $baChart.on('mousemove', function(event) {
   $toolTip2.css({
     left: (event.offsetX || event.originalEvent.layerX) - $toolTip2.width() / 2 - 10,
     top: (event.offsetY || event.originalEvent.layerY) - $toolTip2.height() - 30
+  });
+});
+
+//SLUGGING
+var slugging = {
+  labels: labels,
+  series: [ slugData ]
+};
+
+var slugOptions = {
+  axisX: {
+    showGrid: false
+  },
+  axisY: {
+    showGrid: false,
+    showLabel: false
+  }
+};
+
+var slugChart =  Chartist.Line('.ct-slug', slugging, slugOptions);
+
+//Slug draw
+slugChart.on('draw', function(data) {
+if (data.type === "point") {
+    //Change colors
+    if (data.index < 11 || data.index > 18) {
+      data.element.attr({
+        style: 'stroke: blue;'
+      });
+    }
+  }
+});
+
+//Slug interactives
+
+var $slugChart = $('.ct-slug');
+
+var $toolTip3 = $slugChart
+.append('<div class="tooltip"></div>')
+.find('.tooltip')
+.hide();
+
+$slugChart.on('mouseenter', '.ct-point', function() {
+  var $point = $(this), info = $point.attr('ct:value');
+  var meta = Chartist.deserialize(info);
+
+  $toolTip3.html(info).show();
+});
+
+$slugChart.on('mouseleave', '.ct-point', function() {
+  $toolTip3.hide();
+});
+
+$slugChart.on('mousemove', function(event) {
+  $toolTip3.css({
+    left: (event.offsetX || event.originalEvent.layerX) - $toolTip3.width() / 2 - 10,
+    top: (event.offsetY || event.originalEvent.layerY) - $toolTip3.height() - 30
+  });
+});
+
+//PLAYED 
+var played = {
+  labels: labels,
+  series: [ playData ]
+};
+
+var playOptions = {
+  axisX: {
+    showGrid: false
+  },
+  axisY: {
+    showGrid: false,
+    showLabel: false
+  }
+};
+
+var playChart =  Chartist.Line('.ct-play', played, playOptions);
+
+//Slug draw
+playChart.on('draw', function(data) {
+if (data.type === "point") {
+    //Change colors
+    if (data.index < 11 || data.index > 18) {
+      data.element.attr({
+        style: 'stroke: blue;'
+      });
+    }
+  }
+});
+
+//Slug interactives
+
+var $playChart = $('.ct-play');
+
+var $toolTip4 = $playChart
+.append('<div class="tooltip"></div>')
+.find('.tooltip')
+.hide();
+
+$playChart.on('mouseenter', '.ct-point', function() {
+  var $point = $(this), info = $point.attr('ct:value');
+  var meta = Chartist.deserialize(info);
+
+  $toolTip4.html(info).show();
+});
+
+$playChart.on('mouseleave', '.ct-point', function() {
+  $toolTip4.hide();
+});
+
+$playChart.on('mousemove', function(event) {
+  $toolTip4.css({
+    left: (event.offsetX || event.originalEvent.layerX) - $toolTip4.width() / 2 - 10,
+    top: (event.offsetY || event.originalEvent.layerY) - $toolTip4.height() - 30
   });
 });
 
@@ -198,4 +315,4 @@ $hrChart.on('mousemove', function(event) {
 
 
 //Sports Illustrated
-
+//qsa(".si-img").forEach
